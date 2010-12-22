8eb51e733572c02b9d70c5fd9aec86bd76969c85
Ole Petter Bang <olepbang@gmail.com>
2010-12-22 04:26:34 +0100

    added splitter

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,29 +1,73 @@
-define(['common', 'panel', 'presenter', 'presentation'], 
-  function (common, Panel, Presenter, Presentation) {
+define(['common', 'element', 'panel', 'presenter', 'presentation'], 
+  function (common, Element, Panel, Presenter, Presentation) {
+
+  var panel, splitter, presenter;
 
   function loadEnvironment() {
-    var presentation = new Presentation(),
-        presenter = new Presenter('#presenter', presentation),
-        panel = new Panel('panel', presentation);
+    var presentation = new Presentation();
 
-    loadLayout(presenter, panel);
+    panel = new Panel('panel', presentation);
+    splitter = new Element('#splitter');
+    presenter = new Presenter('#presenter', presentation);
+
+    loadLayout();
+    loadSplitter();
   }
 
-  function loadLayout(presenter, panel) {
+  function loadSplitter() {
+    var resizing, offset;
+
+    splitter.bind('mousedown', function (e) {
+      offset = e.offsetX;
+      resizing = true;
+      splitter.addClass('resizing');
+      $(document.body).css({cursor: 'w-resize'});
+      return false;
+    });
+
+    $(document).bind('mousemove', function (e) {
+      if (resizing) {
+        resize(e.clientX - offset);
+        return false;
+      }
+    });
+
+    $(document).bind('mouseup', function (e) {
+      if (resizing) {
+        resizing = false;
+        splitter.removeClass('resizing');
+        $(document.body).css({cursor: 'inherit'});
+      }
+    });
+  }
+
+  function loadLayout() {
     var container = $(window);
 
-    var updateLayout = function () {
-      var panelWidth = Math.floor(container.width() * 0.33);
+    container.resize(function (e) {
+      resize(panel.innerWidth());
+    });
 
-      panel.resize(0, 0, panelWidth, container.height());
+    resize(Math.floor(container.width() * 0.33));
+  }
 
-      presenter.resize(
-        panel.width(), 0, container.width() - panel.width(),
-        container.height());
+  function resize(panelWidth) {
+    var container = $(window);
+
+    if (panelWidth < container.width() * 0.2) {
+      panelWidth = Math.floor(container.width() * 0.2);
+    }
+    else if (panelWidth > container.width() * 0.8) {
+      panelWidth = Math.floor(container.width() * 0.8);
     }
 
-    container.resize(updateLayout);
-    updateLayout();
+    panel.resize(0, 0, panelWidth, container.height());
+
+    splitter.resizeElement(panel.width(), 0, undefined, 
+      container.width());
+
+    presenter.resize(panel.width(), 0, container.width() - panel.width(), 
+      container.height());
   }
 
   loadEnvironment();
