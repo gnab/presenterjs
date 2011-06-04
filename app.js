9230019ca5bf7bdb95907ce94665ca48c6144485
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2011-06-04 19:58:37 +0200

    started dropbox integration, converted views to jade

---
+++ app.js
@@ -0,0 +1,80 @@
+require.paths.unshift('lib', 'lib/server');
+
+var express = require('express')
+  , config = require('./config')  
+  , fs = require('fs')
+  , bundle = require('bundle')
+  , dropbox = require('dropbox')
+  , colors = require('colors')
+  , crypto = require('crypto')
+  , everyauth = require('everyauth')
+  ;
+
+var bundlePaths = [
+    './public/scripts/vendor'
+  , './lib/client'
+  ]
+  , bundleTarget = './public/scripts/presenter.js';
+
+bundle(bundlePaths, bundleTarget);
+
+var app = module.exports = express.createServer();
+
+app.configure(function () {
+  app.use(express.logger({ 
+    format: ':method :status'.yellow +
+	    ' :url'.white +
+	    ' -' +
+	    ' :response-time'.green +
+	    ' ms'.blue +
+	    ' -' +
+	    ' :remote-addr :user-agent'.cyan +
+	    ' -' +
+	    ' :date'.light_gray
+  }));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: crypto.createHash('sha256').update(Math.random()+'').digest('hex')
+  }));
+  app.use(everyauth.middleware());
+  app.use(app.router);
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade');
+});
+
+everyauth.helpExpress(app);
+
+app.configure('development', function () {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/list', dropbox.secure, dropbox.files, function (req, res, next) {
+  res.render('list', { files: req.data });
+});
+
+app.get('/authorized', function (req, res) {
+  var url = req.session.redirectPath;
+  if (url) {
+    return res.redirect(url);
+  }
+  res.redirect('/');
+});
+
+app.get('/edit/:presentation', dropbox.secure, function (req, res) {
+  var presentation = req.params.presentation;
+  res.render('edit', { 'presentation': presentation });
+});
+
+
+
